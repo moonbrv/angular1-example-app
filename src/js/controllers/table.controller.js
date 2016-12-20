0a4a1bec3b4fcb1fbc660d74d439b6e3054cd87a
Dmitry Palamarchuk <moonbrv@gmail.com>
2016-12-20 23:44:40 +0200

    add sorting table with data to home page

--- src/js/controllers/table.controller.js
+++ src/js/controllers/table.controller.js
@@ -1,5 +1,25 @@
 export default class tableCtrl {
   constructor(usersListService) {
     this.users = usersListService.users
+    this.sortType = 'name'
+    this.sortReverse = false
+  }
+
+  setOrder(type) {
+    if (this.sortType === type) {
+      this.sortReverse = !this.sortReverse
+    } else {
+      this.sortReverse = false
+      this.sortType = type
+    }
+    // console.log(`order by ${this.sortType} reverse is ${this.sortReverse}`)
+  }
+
+  setIcon() {
+    return !this.sortReverse ? 'glyphicon glyphicon-triangle-bottom' : 'glyphicon glyphicon-triangle-top'
+  }
+
+  setShow(type) {
+    return this.sortType === type
   }
 }
