7bda73f7e8e803bc0c323a8d6b9ed65c4f79ec13
Dmitry Palamarchuk <moonbrv@gmail.com>
2017-01-03 20:29:53 +0200

    update adding user, now we will use func in from controller

--- src/js/components/add-user/add-user.controller.js
+++ src/js/components/add-user/add-user.controller.js
@@ -9,4 +9,10 @@ export default class addUser {
   constructor(usersListService) {
     this.srvc = usersListService
   }
+
+  addUser(e, userObj) {
+    e.preventDefault()
+    this.srvc.addUser(userObj)
+    document.getElementById('user-add').reset()
+  }
 }
