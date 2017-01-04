798ac363ee98904aeec0ad9ca9a05fde1a2773ed
Dmitry Palamarchuk <moonbrv@gmail.com>
2017-01-04 18:25:00 +0200

    add validation for username field

--- src/js/components/add-user/add-user.controller.js
+++ src/js/components/add-user/add-user.controller.js
@@ -8,11 +8,37 @@
 export default class addUser {
   constructor(usersListService) {
     this.srvc = usersListService
+    this.errors = {}
+    this.warnings = {}
+  }
+
+  haveMinimalLength(prop, userObj, minSymbols) {
+    this.warnings[prop] = userObj[prop].length < minSymbols ? `You must enter atleast ${minSymbols} symbols` : ''
+  }
+
+  uniqueProp(prop, userObj) {
+    if (typeof userObj[prop] === 'string') {
+      this.errors[prop] = this.srvc.users.filter(x => x[prop].toLowerCase() === userObj[prop].toLowerCase()).length ? ` User with this ${prop} already exist` : ''
+    } else {
+      this.errors[prop] = this.srvc.users.filter(x => x[prop] === userObj[prop]).length ? ` User with this ${prop} already exist` : ''
+    }
+  }
+
+  haveNoErrors() {
+    // for (let val of this.errors) {
+    //   if (val) return false
+    // }
+    // for (let val of this.warnings) {
+    //   if (val) return false
+    // }
+    return true
   }
 
   addUser(e, userObj) {
     e.preventDefault()
-    this.srvc.addUser(userObj)
+    if (this.haveNoErrors()) {
+      this.srvc.addUser(userObj)
+    }
     document.getElementById('user-add').reset()
   }
 }
