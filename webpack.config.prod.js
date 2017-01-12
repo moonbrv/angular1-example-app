e2142f286d00179f4d4a18a492a00d63c27395ae
Dmitry Palamarchuk <moonbrv@gmail.com>
2017-01-12 22:05:24 +0200

    fix: public path

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -27,7 +27,7 @@ module.exports = {
 
   output: {
     path: path.join(__dirname, '/dist'),
-    publicPath: './',
+    publicPath: '/angular1-example-app',
     filename: '[name].bundle-[hash:6].js',
     chunkFilename: '[id].bundle-[hash:6].js'
   },
