46920c31542eddf3822610f0ad931d72ad8529ae
Dmitry Palamarchuk <moonbrv@gmail.com>
2017-01-15 12:48:45 +0200

    add comment about html5 mode

--- src/js/app.config.js
+++ src/js/app.config.js
@@ -1,6 +1,7 @@
 export default function routing($stateProvider, $urlRouterProvider, $locationProvider) {
   'ngInject'
   // html5 mode commented out, because i want serve this app on gh-pages, which not support this option yet.
+  // if you will enable this option, don't forget include <base href="/"> tag inside index.html, in head.
   // $locationProvider.html5Mode(true)
   $urlRouterProvider.otherwise('/')
   $stateProvider
