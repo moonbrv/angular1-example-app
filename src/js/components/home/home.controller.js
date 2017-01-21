b9996c0dd0b88eef69dea372103b18c54066875e
Dmitry Palamarchuk <moonbrv@gmail.com>
2017-01-21 13:50:23 +0200

    remove unused methods and params

--- src/js/components/home/home.controller.js
+++ src/js/components/home/home.controller.js
@@ -10,8 +10,6 @@ export default class homeCtrl {
   constructor(usersListService, $timeout) {
     'ngInject'
     this.usersListService = usersListService
-    this.sortType = 'name'
-    this.sortReverse = false
     this.tableParams = new NgTableParams({
       count: 5
     },
@@ -29,36 +27,6 @@ export default class homeCtrl {
     }, 200)
   }
 
-  /**
-   * function that decide order of sorting of column
-   * if it first click on column - order of sorting will not reverse and set new OrderBy parametr
-   * @param  {string} typeho
-   */
-  setOrder(type) {
-    if (this.sortType === type) {
-      this.sortReverse = !this.sortReverse
-    } else {
-      this.sortReverse = false
-      this.sortType = type
-    }
-  }
-
-  /**
-   * function decide wich class aply to element decided by sorting order
-   * @returns {string} class for display direction of sorting
-   */
-  setIcon() {
-    return !this.sortReverse ? 'glyphicon glyphicon-triangle-bottom' : 'glyphicon glyphicon-triangle-top'
-  }
-
-  /**
-   * function decide visibility of sort icon in column
-   * @param  {string} type
-   * @returns {boolean}
-   */
-  setShow(type) {
-    return this.sortType === type
-  }
   /**
    * function remove user with chosen id from data in service,
    * and sync table with data in service
